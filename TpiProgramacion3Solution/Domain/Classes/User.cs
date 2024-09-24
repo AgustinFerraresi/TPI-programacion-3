7408fffed33459fa271fef5df6af272e12536d15
Agustin <agustinferraresi5@gmail.com>
2024-09-23 23:12:46 -0300

    creo la solucion, armo la estructura y creo la primera clase

---
+++ TpiProgramacion3Solution/Domain/Classes/User.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Classes
+{
+    public abstract class User
+    {
+        protected string Name { get; set }
+        protected int Id { get; set }
+        protected string Email { get; set }
+        protected string Password { get; set }
+        protected int Dni { get; set }
+
+        protected User(string name, int id, string email, string password, int dni)
+        {
+            Name = name;
+            Id = id;
+            Email = email;
+            Password = password;
+            Dni = dni;
+        }
+    }
+}
